4ee7d116f661cdc7e683916d2aef82d9d2d5bd7c
Tanmay <74710526+UNR-MAGGMA@users.noreply.github.com>
2022-10-01 11:41:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,15 @@
 import firebase from "firebase";
-require("@firebase/firestore");
 
 //Paste your firebaseConfig here
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCiyijZE9CGjqpcKTa9aOrCG1Ky_JWqH1E",
+  authDomain: "project-71-50b43.firebaseapp.com",
+  projectId: "project-71-50b43",
+  storageBucket: "project-71-50b43.appspot.com",
+  messagingSenderId: "543361118697",
+  appId: "1:543361118697:web:91dcd84bc30def82c91b78",
+  measurementId: "G-RH4Q78YZP7"
+};
 //
 firebase.initializeApp(firebaseConfig);
 
